6fab4deb166f95d52848926af52e5eef1f79a5df
Daniel Morillo <danifromecuador@gmail.com>
2025-11-21 12:35:48 -0500

    fix(backend): arreglar CORS para poder editar cocteles

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,25 +2,28 @@ import http from 'http'
 import dotenv from 'dotenv'
 import { handleCocktailsRoutes } from './api/routes/cocktails.routes.js'
 
-// Manejar variables de entorno
 dotenv.config()
 
-// Definir el puerto
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
-// Crear el servidor
 const server = http.createServer((req, res) => {
-  // Headers por defecto
   res.setHeader('Content-Type', 'application/json')
 
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
-  // Manejo de rutas
+  // Manejar preflight OPTIONS
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200)
+    res.end()
+    return
+  }
+
   handleCocktailsRoutes(req, res)
 })
 
-// Inicializar el server
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
