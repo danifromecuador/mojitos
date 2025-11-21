a90494ca12663e96f3a405503d342440eb6289e1
Daniel Morillo <danifromecuador@gmail.com>
2025-11-21 09:50:10 -0500

    fix(backend): usar la url de la imagen sin recortar de imgbb

--- backend/src/data/cocktails.data.js
+++ backend/src/data/cocktails.data.js
@@ -2,21 +2,21 @@ export const cocktails = [
   {
     id: "1",
     name: 'Mojito',
-    image: 'https://ibb.co/JWfmxYyM',
+    image: 'https://i.ibb.co/LDLYQTJ2/Mojito.jpg',
     description: 'Un refrescante cóctel cubano elaborado con ron blanco, menta fresca, jugo de limón, azúcar y agua con gas. Perfecto para días cálidos de verano.',
     price: 3
   },
   {
     id: "2",
     name: 'Margarita',
-    image: 'https://ibb.co/kgF1fDkg',
+    image: 'https://i.ibb.co/zhwJY2jh/Margarita.png',
     description: 'Un clásico cóctel mexicano con tequila, triple sec y jugo de limón fresco, servido con borde salado. Refrescante y con un toque cítrico.',
     price: 4
   },
   {
     id: "3",
     name: 'Daiquiri',
-    image: 'https://ibb.co/rRP11cJW',
+    image: 'https://i.ibb.co/zTc007wk/Daiquiri.jpg',
     description: 'Una bebida tropical de Puerto Rico con ron, crema de coco y jugo de piña fresco. Suave, dulce y deliciosa.',
     price: 7
   }
