669c1057c35efbef2134b248b4fb4fbcf45d6e10
Daniel Morillo <danifromecuador@gmail.com>
2025-11-21 10:46:38 -0500

    feat(frontend): añadir navegacion entre lista y detalles usando react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
 import { CocktailList } from './pages/CocktailList'
+import { CocktailDetail } from './pages/CocktailDetail'
 import { SearchBar } from './components/SearchBar'
 import './App.css'
 
 export const App = () => {
-
   return (
     <div className='app'>
-      <h1>Mojito's App</h1>
+      <h1 className='app-title'>Mojito's App</h1>
       <SearchBar />
-      <CocktailList />
-    </div >
+      
+      <Routes>
+        <Route path="/" element={<CocktailList />} />
+        <Route path="/cocktail/:id" element={<CocktailDetail />} />
+      </Routes>
+    </div>
   )
 }
--- frontend/src/components/CocktailCard.jsx
+++ frontend/src/components/CocktailCard.jsx
@@ -1,20 +1,19 @@
+import { Link } from 'react-router-dom'
 import './CocktailCard.css'
 
 export const CocktailCard = ({ cocktail }) => {
   return (
-    <div className='cocktail-card'>
-      <div className="image-favorites">
-        <img src={cocktail.image} alt={cocktail.name} />
-        <button type="button">♥</button>
+    <Link to={`/cocktail/${cocktail.id}`} style={{ textDecoration: 'none' }}>
+      <div className='cocktail-card'>
+        <div className="image-favorites">
+          <img src={cocktail.image} alt={cocktail.name} />
+          <button type="button">♥</button>
+        </div>
+        <div className="name-price">
+          <h2>{cocktail.name}</h2>
+          <span>${cocktail.price}</span>
+        </div>
       </div>
-      <div className="name-price">
-        <h2>{cocktail.name}</h2>
-        <span>${cocktail.price}</span>
-      </div>
-      <div className='actions'>
-        <button type="button">delete</button>
-        <button type="button">edit</button>
-      </div>
-    </div>
+    </Link>
   )
 }
